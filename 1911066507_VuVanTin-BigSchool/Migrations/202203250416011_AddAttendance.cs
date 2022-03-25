f9a8dc29016d50d1470d0324b7f2298fe1460cf8
tinvu23 <tinvu23102001@gmail.com>
2022-03-24 16:52:15 -1200

    ngay 24/03 4h

---
+++ 1911066507_VuVanTin-BigSchool/Migrations/202203250416011_AddAttendance.cs
@@ -0,0 +1,16 @@
+﻿namespace _1911066507_VuVanTin_BigSchool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddAttendance : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
